78cbbd0c8c10e19a96b169372f7db5e83462cbcb
Romain Lalaut <romain.lalaut@laposte.net>
2014-05-01 11:20:07 +0200

    Add options to the helper + add a leading slash to url_for

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -1,9 +1,9 @@
 module Shortener::ShortenerHelper
 
   # generate a url from a url string
-  def short_url(url, owner=nil)
-    short_url = Shortener::ShortenedUrl.generate(url, owner)
-    short_url ? url_for(:controller => :"shortener/shortened_urls", :action => :show, :id => short_url.unique_key, :only_path => false) : url
+  def short_url(url, owner=nil, opts = {})
+    short_url = Shortener::ShortenedUrl.generate(url, owner, opts)
+    short_url ? url_for(:controller => :"/shortener/shortened_urls", :action => :show, :id => short_url.unique_key, :only_path => false) : url
   end
 
 end
